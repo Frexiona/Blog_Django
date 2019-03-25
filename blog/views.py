8876af90e4a03a4c0fe61d1e27ae4ad8b6535a74
Frexiona <frexiona@foxmail.com>
2019-03-25 22:42:10 +0000

    blog'views

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import  HttpRequest
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpReques('<h1></h1>')
+    return HttpResponse('<h1>Blog Home</h1>')
+
+def about(request):
+    return HttpResponse('<h1>Blog About</h1>')
